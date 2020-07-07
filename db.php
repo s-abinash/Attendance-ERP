07d9100fa9addb024db0b29fd3884da935c6a1b6
AbinashS <s.abinash891@gmail.com>
2020-07-07 21:58:33 +0530

    Check

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con= new mysqli("localhost","root","","attendance");
+$con= new mysqli("localhost:3306","admin_abinash","5&3c9qfdchRvmlTX","student");
     if ($con->connect_error)
     {
         die("Connection failed: " . $con->connect_error);
