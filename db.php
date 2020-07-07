d536fe1bc025f71e5da71ca452e1ab63d8821d37
AbinashS <s.abinash891@gmail.com>
2020-07-07 22:06:33 +0530

    DB Commit

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con= new mysqli("localhost:3306","admin_abinash","5&3c9qfdchRvmlTX","student");
+$con= new mysqli("localhost:3306","admin_abinash","5&3c9qfdchRvmlTX","attendance");
     if ($con->connect_error)
     {
         die("Connection failed: " . $con->connect_error);
