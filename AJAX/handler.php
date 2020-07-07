cb988cdd1912d622ee51eece028b87bef61b9111
AJAY <61835820+AjayR07@users.noreply.github.com>
2020-07-08 02:13:47 +0530

    Update handler.php for Home.php

--- AJAX/handler.php
+++ AJAX/handler.php
@@ -14,6 +14,7 @@
             session_start();
             $_SESSION["id"]=$row['staffid'];
             $_SESSION["name"]=$row['name'];
+            $_SESSION["dept"]=$row['dept'];
             echo "Success";
        }
        else
@@ -23,4 +24,43 @@
       exit();
        
     }
+    else if(isset($_POST["tab"]))
+    {
+         $tab=strtolower($_POST["tab"]);
+         $code=$_POST["code"];
+         $sql="SELECT * FROM `tt` WHERE `class` LIKE '$tab'";
+         $res=$con->query($sql);
+         $day=array();
+         while($row=$res->fetch_assoc())
+         { 
+              $per = array($row["1"],$row["2"], $row["3"],$row["4"],$row["5"]);
+              if(in_array($code, $per))
+              {
+               array_push($day,$row["day"]);
+              }
+         }
+         $x=date("Y-m-d");
+         $tdy=date_create($x);
+         $date=date("2020-07-08");
+         $diff=intval(date_diff($tdy,date_create($date))->format("%a"))+1;
+         $dates=array();
+     
+         for($i=1;$i<=$diff;$i++)
+         {    
+              $s=date("l", strtotime($date));
+               if(in_array($s,$day))
+               {
+                    $sql="SELECT * FROM `$tab` where date LIKE '$date' AND code LIKE '$code'"; 
+                    $r=$con->query($sql);
+                    if($r->num_rows==0)
+                    {    
+                         array_push($dates,$date);
+                    }
+               }
+               $date=date_format(date_add(date_create($date),date_interval_create_from_date_string("1 days")),"Y-m-d");
+         }
+          echo json_encode($dates);
+
+    }
 ?>
+
