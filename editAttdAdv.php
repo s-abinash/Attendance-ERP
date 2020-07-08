fbcd621bb59a3ceb10e01c59354258e397936a61
AbinashS <s.abinash891@gmail.com>
2020-07-08 23:13:34 +0530

    Edit Front End Done

--- editAttdAdv.php
+++ editAttdAdv.php
@@ -11,6 +11,8 @@ if(!isset($_SESSION['id']))
 // }
 include_once('db.php');
 $batch=$_SESSION['batch'];
+$sec=$_SESSION['sec'];
+$dep=$_SESSION['dept'];
 
 ?>
 <html lang="en">
@@ -45,6 +47,7 @@ include_once('./navbar.php');
         });
 
         $('#code').dropdown();
+        $('#hr').dropdown();
     });
     </script>
 </head>
@@ -54,7 +57,100 @@ include_once('./navbar.php');
     body {
         background: url('./images/bgpic.jpg');
     }
+
+
+    #card {
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
     </style>
+    <?php
+
+if(isset($_POST['fetch']))
+{
+    $date=date("Y-m-d", strtotime(str_replace('/', '-', $_POST['date'])));
+    $code=$_POST['code'];
+    $period=$_POST['hrs'];
+    $class=strval(intval($batch)-2000).'-cse-'.strtolower($sec);
+    $sql="SELECT * from `".$class."` WHERE `date` like '$date' and `code` like '$code' and `period` like '$period'";
+    echo $sql;
+    $data=$con->query($sql);
+    if($data->num_rows==0)
+    {
+        echo "<script> Notiflix.Report.Failure( 'Record Not Found', 'Nothing Available for the given data', 'Okay' );</script>";
+    }
+    else
+    {
+        $_SESSION['date']=$date;
+        $_SESSION['code']=$code;
+        $_SESSION['period']=$period;
+    }
+
+    
+    
+}
+?>
+    <?php 
+if(isset($_POST['fetch']))
+{
+    echo '<script>
+            $(document).ready(function(){
+                $(".card-1").css("display", "none");
+            });
+            $(document).ready(function(){
+                $(".card-2").css("display", "");
+            });
+            </script>';
+}
+?>
+    <?php 
+if(isset($_POST['finalize']))
+{
+    if($_POST['finalize']=="done")
+    {
+        
+        foreach($hrs as $h)
+        {
+            $asst=$_SESSION['assoc'];
+            $into='(`date`,`code`,`period`,';
+            $vals='("'.$date.'","'.$code.'","'.$h.'",';
+            foreach($asst as $roll=>$at)
+            {
+                $into.='`'.$roll.'`,';
+                $vals.='"'.$at.'",';
+            }
+            $into=substr($into,0,-1).')';
+            $vals=substr($vals,0,-1).');';
+            $sql="INSERT INTO `".$class."` ".$into." VALUES ".$vals;
+            
+            $con->query($sql);
+        }
+        
+        unset($_SESSION['array1']);
+        unset($_SESSION['array2']);
+        unset($_SESSION['array3']);
+        unset($_SESSION['assoc']);
+        unset($_SESSION['upload']);
+        echo "<script>Notiflix.Report.Success('Success','Attendance Marked Successfully','Okay',function(){window.location.replace('home.php');});</script>";
+        exit();
+    }
+    else
+    {
+            unset($_SESSION['array1']);
+            unset($_SESSION['array2']);
+            unset($_SESSION['array3']);
+            unset($_SESSION['assoc']);
+            unset($_SESSION['upload']);
+            echo "<script>window.location.replace('./home.php');</script>";
+          
+    }
+        
+}
+?>
     <div class="card-1">
         <div class="ui raised padded container segment" id="card" style="margin:auto;width:60%;">
             <center>
@@ -90,10 +186,18 @@ include_once('./navbar.php');
 
                         </select>
                     </div>
-
+                    <div class="field">
+                        <label>Period: </label>
+                        <select name="hrs" class="ui search dropdown" id="hr" required>
+                            <option value="">Select Period</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
                 </div>
-
-
                 <div class="field">
                     <center><button class="ui positive button" type="submit" name="fetch">Fetch</button></center>
                 </div>
@@ -103,42 +207,95 @@ include_once('./navbar.php');
 
 
     <div class="card-2" style="display: none">
-        <div class="ui raised padded container segment" id="card" style="margin:auto;width:60%;">
+        <div class="ui raised padded container segment" id="card" style="height:80%;overflow:auto;width:60%;">
             <center>
                 <h1 class="header">
                     Edit Attendance
                 </h1>
+                <div class="description">
+                    Color Diff refers changes that are currently made
+                </div>
             </center>
-            <?php
+            <form class="ui form" name="edit" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+                <div class="content">
+                    <table class="ui compact table">
+                        <thead>
+                            <tr>
+                                <th colspan="3">
+                                    Attendance Report
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+            $date=$_SESSION['date'];
+            $code=$_SESSION['code'];
+            $period=$_SESSION['period'];
             $sql="SELECT regno from registration where batch like '$batch' and sec like '$sec' and dept like '$dep'";
             $data=$con->query($sql);
-            $sql1="SELECT * from ";
+            $class=strval(intval($batch)-2000).'-cse-'.$sec;
+            $sql1="SELECT * from `".$class."` WHERE `date` like '$date' and `code` like '$code' and `period` like '$period'";
+            $data1=$con->query($sql1);
+            $row=$data1->fetch_assoc();
             while($r=mysqli_fetch_array($data))
             {  
                 $r=$r['regno'];
                 $mark='';
                 $cls='';
-                if((in_array($r,$arr1)&&in_array($r,$arr2))||(in_array($r,$arr2)&&in_array($r,$arr3))||(in_array($r,$arr1)&&in_array($r,$arr3)))
+                $check='';
+                if($row[$r]=='P')
                 {
-                    $attend[$r]='P';
                     $mark='<i class="large green checkmark icon"></i>';
                     $cls='positive';
+                    $check='checked';
                 }    
                 else
                 {    
-                    $attend[$r]='A';
                     $mark='<i class="large red times icon"></i>';
                     $cls='negative';
                 }
                 echo '<tr>
-                <td class="'.$cls.'">
+                <td><div class="ui toggle checkbox">
+                <input type="checkbox" name="'.$r.'" '.$check.'>
+                <label></label>
+                </div></td>
+                <td id="'.$r.'" class="'.$cls.'">
                     '.$r.'
                 </td>
-                <td class="'.$cls.'">'.$mark.'</td>
+                <td id="'.$r.'1'.'" class="'.$cls.'">'.$mark.'</td>
+                
             </tr>';
             }
-                ?>
+            ?>
+                        </tbody>
+                    </table>
+
+                </div>
+                <button class="ui positive button" name="finalize" value="done" style="float:right;">Finalize</button>
+                <button class="ui negative button" name="finalize" value="goback" style="float:left;">Go Back</button>
+            </form>
         </div>
+        <script>
+        $(document).ready(function() {
+            $('input[type="checkbox"]').on('change', function() {
+                var temp = $(this).attr("name");
+                var ele = document.getElementById(temp);
+                $(ele).toggleClass('positive', $(this).is(':checked'));
+
+                $(ele).toggleClass('negative', $(this).not(':checked'));
+
+                if ($(this).is(':checked')) {
+                    var temp = $(this).attr("name") + 1;
+                    var ele = document.getElementById(temp);
+                    $(ele).children().attr("class", 'large green checkmark icon');
+                } else {
+                    var temp = $(this).attr("name") + 1;
+                    var ele = document.getElementById(temp);
+                    $(ele).children().attr("class", 'large red times icon');
+                }
+            });
+        });
+        </script>
 </body>
 
 </html>
