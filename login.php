ebbecdf457ce3a9664fe48cb9d24f78af516300f
AbinashS <s.abinash891@gmail.com>
2020-07-10 08:52:06 +0530

    Updated Login

--- login.php
+++ login.php
@@ -6,47 +6,47 @@ if(isset($_SESSION['id']))
 }
 ?>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="icon" type="image/png" href="./images/KEC.png">
-    <link rel="stylesheet" href="./assets/Fomantic/dist/semantic.min.css" type="text/css"/> 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+    <link rel="stylesheet" href="./assets/Fomantic/dist/semantic.min.css" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css" />
     <style>
-    body
-    {
-        margin:0;
-        padding:0;
+    body {
+        margin: 0;
+        padding: 0;
         font-family: sans-serif;
-        background:url("./images/bgpic.jpg") ;
+        background: url("./images/bgpic.jpg");
         background-size: cover;
     }
-    .box
-    {
-        position:absolute;
+
+    .box {
+        position: absolute;
         top: 50%;
         left: 50%;
-        transform:translate(-50%,-50%);
-        width:400px;
-        padding:40px;
-        background:rgba(0,0,0,.8);
+        transform: translate(-50%, -50%);
+        width: 400px;
+        padding: 40px;
+        background: rgba(0, 0, 0, .8);
         box-sizing: border-box;
-        box-shadow: 0 15px 25px rgba(0,0,0,.5);
+        box-shadow: 0 15px 25px rgba(0, 0, 0, .5);
         border-radius: 10px;
     }
-    .box h2
-    {
+
+    .box h2 {
         margin: 0 0 30px;
         padding: 0;
         color: #fff;
         text-align: center;
     }
-    .box .inputBox
-    {
-        position:relative;
+
+    .box .inputBox {
+        position: relative;
     }
-    .box .inputBox input
-    {
+
+    .box .inputBox input {
         width: 100%;
         padding: 10px 0;
         font-size: 16px;
@@ -58,8 +58,8 @@ if(isset($_SESSION['id']))
         outline: none;
         background: transparent;
     }
-    .box .inputBox label
-    {
+
+    .box .inputBox label {
         position: absolute;
         top: 0;
         left: 0;
@@ -69,16 +69,16 @@ if(isset($_SESSION['id']))
         pointer-events: none;
         transition: .5s;
     }
-    .box .inputBox input:focus ~ label,
-    .box .inputBox input:valid ~ label
-    {
+
+    .box .inputBox input:focus~label,
+    .box .inputBox input:valid~label {
         top: -18px;
         left: 0;
         color: #03a9f4;
         font-size: 12px;
     }
-    .box input[type="submit"]
-    {
+
+    .box input[type="submit"] {
         background: transparent;
         border: none;
         outline: none;
@@ -88,12 +88,22 @@ if(isset($_SESSION['id']))
         cursor: pointer;
         border-radius: 5px;
     }
-    
-</style>
+
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 8%;
+        background-color: black;
+        color: white;
+        text-align: center;
+    }
+    </style>
 </head>
 
 <body>
-<?php include_once('./assets/notiflix.php'); ?>
+    <?php include_once('./assets/notiflix.php'); ?>
     <div class="box">
         <h2 class="animate__animated animate__bounce "> Staff Login</h2>
         <form id="login">
@@ -105,48 +115,54 @@ if(isset($_SESSION['id']))
                 <input type="password" name="pass" required>
                 <label>Password</label>
             </div>
+            <div style="float:left;color:pink;">
+                <a href="mailto:studentplus@kongu.edu?subject=Attendance Reg.," target="_blank">Contact Admin</a>
+            </div><br /><br />
             <center>
                 <button type="submit" id="sub" class="ui positive button">Sign in</button>
-            </center>           
+            </center>
         </form>
     </div>
- 
-  <script src="./assets/jquery.min.js"></script>
-  <script src="./assets/Fomantic/dist/semantic.min.js"></script>
-<script>
-$(document).ready(function(){
-  
-    $("#login").on("submit",function(){
-        $("#sub").addClass("loading");
-        var frm=$("#login").serialize();
-        frm+="&usr=verified";
- 
-        $.ajax({
-            url:"./AJAX/handler.php",
-            data:frm,
-            type:"POST",
-            success:function(res)
-            {
-               
-                $("#sub").removeClass("loading");
-                if(res=="Success")
-                {
-                    Notiflix.Notify.Success("Logged in Successfully");
-                    window.location.replace("home.php");
-                }
-                else
-                {
-                    Notiflix.Notify.Failure("Credential Mismatch");
-                    $("#login")[0].reset();
-                }
-            }
+    <div class="footer">
+        <p style="vertical-align: middle;padding: 10px;" id="aboutus"> Website developed by
+            <span style="color:violet;">Abinash S</span> and <span style="color:violet;">Ajay R </span></p>
+    </div>
+
+    <script src="./assets/jquery.min.js"></script>
+    <script src="./assets/Fomantic/dist/semantic.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $("#aboutus").on("click", function() {
+            window.location.href = "https://kecstudent.xyz/aboutUs";
         });
+        $("#login").on("submit", function() {
+            $("#sub").addClass("loading");
+            var frm = $("#login").serialize();
+            frm += "&usr=verified";
 
-        return false;
+            $.ajax({
+                url: "./AJAX/handler.php",
+                data: frm,
+                type: "POST",
+                success: function(res) {
 
+                    $("#sub").removeClass("loading");
+                    if (res == "Success") {
+                        Notiflix.Notify.Success("Logged in Successfully");
+                        window.location.replace("home.php");
+                    } else {
+                        Notiflix.Notify.Failure("Credential Mismatch");
+                        $("#login")[0].reset();
+                    }
+                }
+            });
+
+            return false;
+
+        });
     });
-});
-</script>   
-    
+    </script>
+
 </body>
+
 </html>
