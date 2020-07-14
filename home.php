d86eef0c82f8f5bfabaea3f026ba126406c57414
AbinashS <s.abinash891@gmail.com>
2020-07-14 10:24:08 +0530

    Changed Consolidation Button Color

--- home.php
+++ home.php
@@ -18,13 +18,13 @@
 
 <body>
     <style>
-        body 
-        {
-            background-image: url("./images/bgpic.jpg");
-        }
+    body {
+        background-image: url("./images/bgpic.jpg");
+    }
     </style>
     <div id="tabl">
-    <div class="ui header" style="text-align:center;font-size:30px;margin-top:2%;color:#ADEFD1FF">Your Class Associations</div>
+        <div class="ui header" style="text-align:center;font-size:30px;margin-top:2%;color:#ADEFD1FF">Your Class
+            Associations</div>
         <table class="ui selectable striped  table" style="margin:auto;width:80%;margin-top:5%">
             <thead>
                 <tr style="color:black;font-size:20px" class="center aligned">
@@ -77,7 +77,7 @@
                     <td>'.$sec.'</td>
                     <td>'.$code.'</td>
                     <td>'.$name.'</td>
-                    <td class="right aligned"><button class="ui primary right icon button" id="'.$btn.'" onclick="attend(this.id)"> Mark Attendance &nbsp&nbsp<i class="check icon"></i></button>&nbsp;&nbsp;<button class="ui black right icon button" id="'.$btn.'" onclick="history(this.id)"> View History &nbsp&nbsp<i class="history icon"></i></button><button class="ui black right icon button" id="'.$btn.'" onclick="consolidate(this.id)"> Consolidation &nbsp&nbsp<i class="file export icon"></i></button></td>
+                    <td class="right aligned"><button class="ui primary right icon button" id="'.$btn.'" onclick="attend(this.id)"> Mark Attendance &nbsp&nbsp<i class="check icon"></i></button>&nbsp;&nbsp;<button class="ui black right icon button" id="'.$btn.'" onclick="history(this.id)"> View History &nbsp&nbsp<i class="history icon"></i></button><button class="ui brown right icon button" id="'.$btn.'" onclick="consolidate(this.id)"> Consolidation &nbsp&nbsp<i class="file export icon"></i></button></td>
                     </tr>';
                     }
                     if($lab!="")
@@ -111,7 +111,7 @@
                     <td>'.$sec.'</td>
                     <td>'.$code.'</td>
                     <td>'.$name.'</td>
-                    <td class="right aligned"><button class="ui primary right icon button " id="'.$btn.'"  onclick="attend(this.id)"> Mark Attendance &nbsp&nbsp<i class="check icon"></i></button><button class="ui black right icon button" id="'.$btn.'" onclick="history(this.id)"> View History &nbsp&nbsp<i class="history icon"></i></button><button class="ui black right icon button" id="'.$btn.'" onclick="consolidate(this.id)"> Consolidation &nbsp&nbsp<i class="file export icon"></i></button></td>
+                    <td class="right aligned"><button class="ui primary right icon button " id="'.$btn.'"  onclick="attend(this.id)"> Mark Attendance &nbsp&nbsp<i class="check icon"></i></button><button class="ui black right icon button" id="'.$btn.'" onclick="history(this.id)"> View History &nbsp&nbsp<i class="history icon"></i></button><button class="ui brown right icon button" id="'.$btn.'" onclick="consolidate(this.id)"> Consolidation &nbsp&nbsp<i class="file export icon"></i></button></td>
                     
                 </tr>';
                 }
@@ -119,8 +119,8 @@
                 ?>
             </tbody>
         </table>
-            </div>
-           
+    </div>
+
     <div id="seg" style="display:none">
     </div>
 
@@ -128,7 +128,7 @@
 
 
 
-<div class="ui tiny inverted modal" id="datepickermod">
+    <div class="ui tiny inverted modal" id="datepickermod">
         <div class="header">
             KEC Student+
             <button class="ui right floated negative icon button" id="closebtn1">
@@ -189,141 +189,138 @@
             </button>
             </form>
         </div>
-</div>
-        <script>
-        var d = "";
+    </div>
+    <script>
+    var d = "";
 
-        function attend(id) {
-            var btn = id.split("/");
-            d = "tab=" + btn[0] + "&code=" + btn[1];
-            $.ajax({
-                url: "./AJAX/handler.php",
-                data: d,
-                type: "POST",
-                success: function(res) {
-                    var arr = [];
-                    var i;
-                    var dates = JSON.parse(res);
-                    if (!(Array.isArray(dates) && dates.length)) {
-                        Notiflix.Notify.Info("You have no pending Attendance reports to be uploaded");
-                        return false;
-                    }
-                    for (i of dates) {
-                        var r = i.split("-");
-                        arr.push(new Date(r[0], r[1] - 1, r[2]));
-                    }
-                    $('#cal').calendar({
-                        type: 'date',
-                        enabledDates: arr,
-                        formatter: {
-                            date: function(date, settings) {
-                                if (!date) return '';
-                                var day = date.getDate();
-                                var month = date.getMonth() + 1;
-                                var year = date.getFullYear();
-                                return day + '/' + month + '/' + year;
-                            }
+    function attend(id) {
+        var btn = id.split("/");
+        d = "tab=" + btn[0] + "&code=" + btn[1];
+        $.ajax({
+            url: "./AJAX/handler.php",
+            data: d,
+            type: "POST",
+            success: function(res) {
+                var arr = [];
+                var i;
+                var dates = JSON.parse(res);
+                if (!(Array.isArray(dates) && dates.length)) {
+                    Notiflix.Notify.Info("You have no pending Attendance reports to be uploaded");
+                    return false;
+                }
+                for (i of dates) {
+                    var r = i.split("-");
+                    arr.push(new Date(r[0], r[1] - 1, r[2]));
+                }
+                $('#cal').calendar({
+                    type: 'date',
+                    enabledDates: arr,
+                    formatter: {
+                        date: function(date, settings) {
+                            if (!date) return '';
+                            var day = date.getDate();
+                            var month = date.getMonth() + 1;
+                            var year = date.getFullYear();
+                            return day + '/' + month + '/' + year;
                         }
-                    });
-                    $('#hr').dropdown({
-                        maxSelections: 3
-                    });
-                    $('#tab').val(btn[0]);
-                    $('#code').val(btn[1]);
-                    $("#datepickermod").modal({
-                        centered: false
-                    }).modal("show");
-                }
-            });
-        }
-        function history(id)
-        {
-            var btn1 = id.split("/");
-            d1 = "cls=" + btn1[0] + "&code=" + btn1[1];
-            $.ajax({
-                url: "./AJAX/handler.php",
-                data: d1,
-                type: "POST",
-                success: function(r) {
-                    if(r=== '')
-                    {             
-                        Notiflix.Notify.Info("You haven't uploaded any Attendance reports yet");
-                        return false;
-                    }        
-                    $("#seg").html("");
-                    $("#seg").append('<button class="ui right floated circular teal icon button" id="bt" data-tooltip="Back to Home" data-position="bottom right" data-inverted=""  onclick="clss()" style="margin-right:1%;"><i class="undo icon"></i></button><div class="ui header" style="text-align:center;font-size:30px;margin-top:2%;color:#ADEFD1FF">Attendance History &nbsp;&nbsp;<i class="history icon"></i></div>'+r);
-                    $("#tabl").hide();
-                    $("#seg").show();    
-                    Notiflix.Notify.Info("Hover on Absentees count to the view Absentees List");
-                }
-            })
-        }
-
-
-        function consolidate(id)
-        {
-            var btn2 = id.split("/");
-            d2 = "tname=" + btn2[0] + "&ccode=" + btn2[1]+"&consolidate=true";
-            $.ajax({
-                url: "./AJAX/handler.php",
-                data: d2,
-                type: "POST",
-                success: function(r) {
-                    if(r=='empty')
-                    {  
-                                
-                        Notiflix.Notify.Info("You haven't uploaded any Attendance reports yet to consolidate");
-                        return false;
-                    } 
-                    else if(r=="export_ready")    
-                    {
-                        window.location.href="export.php";
-                    }  
-                    else
-                    {
-                        Notiflix.Notify.Warning("Error in retrieving data.Please try again Else Contact Admin");
                     }
-                    
-                }
-            })
-        }
-    
-        $(document).ready(function() {
-            
-            $("#datepickermod").modal({});
-            $("#closebtn1").click(function() {
-                $("#datepickermod").modal("hide");
-            });
-            $('#cal').calendar({
-                type: 'date'
-            });
+                });
+                $('#hr').dropdown({
+                    maxSelections: 3
+                });
+                $('#tab').val(btn[0]);
+                $('#code').val(btn[1]);
+                $("#datepickermod").modal({
+                    centered: false
+                }).modal("show");
+            }
         });
-        function clss()
-        {   
-            $("#tabl").show();
-            $("#seg").hide();  
-        }
-        function editor(val)
-        {
-            var e = val.split("/");
-            var arr={"e_code":e[0],"e_date":e[1],"e_period":e[2],"edittab":e[3],"editor":"edit"};
-            $.ajax({
-                url:"./AJAX/handler.php",
-                type:"POST",
-                data:arr,
-                success:function(r)
-                {
-                    if(r=="go&edit")
-                    {
-                        window.location.href="editAttdAdv.php";
-                    }                   
+    }
+
+    function history(id) {
+        var btn1 = id.split("/");
+        d1 = "cls=" + btn1[0] + "&code=" + btn1[1];
+        $.ajax({
+            url: "./AJAX/handler.php",
+            data: d1,
+            type: "POST",
+            success: function(r) {
+                if (r === '') {
+                    Notiflix.Notify.Info("You haven't uploaded any Attendance reports yet");
+                    return false;
                 }
-            });
-        }
-      
-        
-        </script>
-        
+                $("#seg").html("");
+                $("#seg").append(
+                    '<button class="ui right floated circular teal icon button" id="bt" data-tooltip="Back to Home" data-position="bottom right" data-inverted=""  onclick="clss()" style="margin-right:1%;"><i class="undo icon"></i></button><div class="ui header" style="text-align:center;font-size:30px;margin-top:2%;color:#ADEFD1FF">Attendance History &nbsp;&nbsp;<i class="history icon"></i></div>' +
+                    r);
+                $("#tabl").hide();
+                $("#seg").show();
+                Notiflix.Notify.Info("Hover on Absentees count to the view Absentees List");
+            }
+        })
+    }
+
+
+    function consolidate(id) {
+        var btn2 = id.split("/");
+        d2 = "tname=" + btn2[0] + "&ccode=" + btn2[1] + "&consolidate=true";
+        $.ajax({
+            url: "./AJAX/handler.php",
+            data: d2,
+            type: "POST",
+            success: function(r) {
+                if (r == 'empty') {
+
+                    Notiflix.Notify.Info("You haven't uploaded any Attendance reports yet to consolidate");
+                    return false;
+                } else if (r == "export_ready") {
+                    window.location.href = "export.php";
+                } else {
+                    Notiflix.Notify.Warning("Error in retrieving data.Please try again Else Contact Admin");
+                }
+
+            }
+        })
+    }
+
+    $(document).ready(function() {
+
+        $("#datepickermod").modal({});
+        $("#closebtn1").click(function() {
+            $("#datepickermod").modal("hide");
+        });
+        $('#cal').calendar({
+            type: 'date'
+        });
+    });
+
+    function clss() {
+        $("#tabl").show();
+        $("#seg").hide();
+    }
+
+    function editor(val) {
+        var e = val.split("/");
+        var arr = {
+            "e_code": e[0],
+            "e_date": e[1],
+            "e_period": e[2],
+            "edittab": e[3],
+            "editor": "edit"
+        };
+        $.ajax({
+            url: "./AJAX/handler.php",
+            type: "POST",
+            data: arr,
+            success: function(r) {
+                if (r == "go&edit") {
+                    window.location.href = "editAttdAdv.php";
+                }
+            }
+        });
+    }
+    </script>
+
 </body>
 
 </html>
