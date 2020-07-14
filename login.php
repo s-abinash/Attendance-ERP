7bf8a0ebdcf6d9693a43ff4acf935c521e6c8c86
AbinashS <s.abinash891@gmail.com>
2020-07-14 11:27:35 +0530

    Changes

--- login.php
+++ login.php
@@ -8,6 +8,8 @@ if(isset($_SESSION['id']))
 <html lang="en">
 
 <head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="icon" type="image/png" href="./images/KEC.png">
