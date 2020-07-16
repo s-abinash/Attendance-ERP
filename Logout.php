aeaeae56dea29f45c0cdd21d2ccda439ba9b2156
AbinashS <s.abinash891@gmail.com>
2020-07-17 00:04:44 +0530

    Logout Changes

--- Logout.php
+++ Logout.php
@@ -3,37 +3,40 @@
     include_once('./assets/notiflix.php');
 ?>
 <html>
-    <head>
+
+<head>
     <meta charset="UTF-8">
     <title>Logout</title>
- 
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./assets/Fomantic/dist/semantic.min.css" type="text/css" />
-  <script src="./assets/jquery.min.js"></script>
-  <script src="./assets/Fomantic/dist/semantic.min.js"></script>
-  <link rel="icon" type="image/png" href="./images/KEC.png">
-  <link rel="stylesheet" type="text/css" href="./assets/animate.min.css"/>
-  <!-- No Script Part -->
-	<noscript><meta http-equiv="refresh" content="0; URL='./errorfile/noscript.html'" /></noscript>
-	<!-- -------- -->
+    <script src="./assets/jquery.min.js"></script>
+    <script src="./assets/Fomantic/dist/semantic.min.js"></script>
+    <link rel="icon" type="image/png" href="./images/KEC.png">
+    <link rel="stylesheet" type="text/css" href="./assets/animate.min.css" />
+    <!-- No Script Part -->
+    <noscript>
+        <meta http-equiv="refresh" content="0; URL='./errorfile/noscript.html'" /></noscript>
+    <!-- -------- -->
     <?php require_once('./assets/notiflix.php');?>
     <script>
-        $(document).ready(function(){
-            $('.ui.card').transition('drop');
-            $('.ui.card').transition('drop');
-            });
-</script>
+    $(document).ready(function() {
+        $('.ui.card').transition('drop');
+        $('.ui.card').transition('drop');
+    });
+    </script>
 </head>
+
 <body>
-<style>
-body{
-    background-image:url('./images/bgpic.jpg');
-}
-</style>
+    <style>
+    body {
+        background-image: url('./images/bgpic.jpg');
+    }
+    </style>
 
 
-<?php
+    <?php
 $status='';
 if(isset($_SESSION['id']))
 {
@@ -51,50 +54,48 @@ else{
    
 }
 ?>
-<center>
-<div class="animated zoomIn">    
-<div class="ui container">
-    <div class="ui card">
-    <div class="image" style="height:40%">
-    <img src="./images/tick-box.svg">
-    </div>
-    <div class="content">
-    
-      <a class="header"><?php echo $status; ?></a>
-     
-      <div class="description">
-       You have logged out out successfuly.<br> We wish to have your valuable feedback.<br> Please spare 5 minutes and give your opinion.
-      </div>
-    </div>
-    <div class="extra content">
-    <center><div class="ui form">
-      <div class="two fields">
-      <div class="field"><a href="https://https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__hxfHbFUOFNCUEFCVzQ1TERNTThDRVFFRk1FVzZJNi4u">
-      <button class="ui primary button">Fill Feedback</button></a></div>
-      <div class="field"><button onclick="window.open('./login.php', '_self');" class="ui secondary button">Close</button></div>
-    </div></center></div>
-    <div class="ui bottom attached positive button" onclick="window.open('./login.php', '_self');">
-      <i class="sign language icon"></i>
-      Thank You!
-    </div>
-  </div></div>
-</div>
-</center>
-  <style>
-.ui.container{
-    /* margin-top: 60px; */
-}
-.ui.card{
-    width: 400px;
-   
-}
-.image{
-    height: 100px;
-}
-img{
-    width: auto;
-     height: auto;}
-</style>
+    <center>
+        <div class="animated zoomIn">
+            <div class="ui container">
+                <div class="ui card">
+                    <div class="image" style="height:40%">
+                        <img src="./images/tick-box.svg">
+                    </div>
+                    <div class="content">
+                        <a class="header"><?php echo $status; ?></a>
+                        <div class="description">
+                            You have logged out out successfuly.<br> Have a Nice Day!.<br>
+                        </div>
+                    </div>
+                    <div class="extra content">
+                        <div class="ui bottom attached black button" onclick="window.open('./login.php', '_self');">
+                            <i class="sign language icon"></i>
+                            Thank You!
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </center>
+    <style>
+    .ui.container {
+        /* margin-top: 60px; */
+    }
+
+    .ui.card {
+        width: 400px;
+
+    }
+
+    .image {
+        height: 100px;
+    }
+
+    img {
+        width: auto;
+        height: auto;
+    }
+    </style>
 
 </body>
+
 </html>
