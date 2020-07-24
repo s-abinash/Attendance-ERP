6b784090dbab9c91602bea0da2a9c49561ad56db
Abinash S <s.abinash@outlook.com>
2020-07-24 23:19:31 +0530

    Sample Commit

--- announcement.php
+++ announcement.php
@@ -9,14 +9,19 @@ $(document).ready(function(){
 </script>
 <div class="ui longer modal">
 <i class="close icon"></i>
-  <div class="header">Announcement</div>
+  <div class="header">New Updates</div>
   <div class="image content">
-    <img class="image" src="./images/announcement.jpg">
-    <div class="description">
+  <!-- <div class="ui vertical banner test ad"> -->
+    <img class="image" src="./images/ann.jpg" style="height: 300px; width: 200px;">
+    <!-- </div> -->
+    <div class="description" style="overflow: auto;">
     <div class="ui bulleted list">
-        <h3 class="item">Adding Meeting URL is Under Work</h3>
-        <h3 class="item">You can now export the attendance Data to Various Formats.</h3>
+        <h3 class="item">Now as a Progressive Web App</h3>
+        <p>In Laptop:</p>
+        
+        <h3 class="item">Google Form auto fill will be enabled by Monday</h3>
         <h3 class="item">Staff can alter their period to another staff. Check it out in the NavBar</h3>
+        <h3 class="item">You can now export the attendance Data to Various Formats.</h3>
         <h3 class="item">Staff altered by someone can see a Red Stripe in Calendar and mark attendance.</h3>
     </div>
   </div>
