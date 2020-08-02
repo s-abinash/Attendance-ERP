55890aad716b4275f74e0a502f4db01ef2a9e31a
AJAY <61835820+AjayR07@users.noreply.github.com>
2020-08-02 23:07:15 +0530

    Update adModules.php

--- AJAX/adModules.php
+++ AJAX/adModules.php
@@ -86,7 +86,7 @@
      
          $x=date("Y-m-d");
          $tdy=date_create($x);
-         $date=date("2020-07-08");
+         $date=date("2020-08-03");
          $diff=intval(date_diff($tdy,date_create($date))->format("%a"))+1;
          $diff+=30;
          $dates=array();
