0428ccc95eb8018e45d44d1ba79a05f59df88e8f
Abinash S <s.abinash@outlook.com>
2020-08-24 14:17:08 +0530

    SEO Update

--- login.php
+++ login.php
@@ -13,6 +13,21 @@ include_once("./db.php");
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
+
+    <!-- SEO -->
+    <meta name="description" content="KEC Student+, an all in one student portal of Kongu Engineering College
+    KEC student + is a promising e-platform that serves both faculty and students in a better way with enhanced UI( user interfaces)and UX( user experience). 
+    This Automated platform is a better alternative to conventional pen paper based applications. Using this platform students can apply on-duties and get it  
+    approved by faculties on time with ease. Students can also keep track of their approved or declined status of their applications. Faculties can review their 
+    application and accept or deny the student request online. This application maintains an e-repository where the certificates submitted by the students are 
+    stacked and made available to both faculties and respective students for their future retrieval">
+    <meta name="copyright" content="KEC Student+ is a trademark of students of KEC, Abinash S, Ajay R, Arul Prasath V">
+    <meta name="keywords" content="KEC,kec,Kongu Engineering College,Perundurai,KEC Student+,KEC CSE,Abinash,Ajay,Arulprasath,Kongu,KEC Attendance,Computer Science,Kongu Erode,KEC Erode,KEC Perndurai,
+    KEC Thopupalayam,KVITT,KEC Student,studentplus,Computer Science KEC,kongu.edu,kongu.ac.in,kongu,keccse,KEC CSE A,studentplus@kongu.edu,kecstudent.xyz">
+    <meta name="author" content="Abinash S, Ajay R, Arul Prasath V">
+    <meta name=”robots” content="index, follow">
+    <!--  -->
+
     <!-- PWA Part -->
     <link rel="manifest" href="./manifest.json">
     <meta name="mobile-web-app-capable" content="yes">
@@ -151,7 +166,7 @@ include_once("./db.php");
                 <label>Password</label>
             </div>
             <div style="float:left;color:pink;">
-                <a href="mailto:studentplus@kongu.ac.in?subject=Attendance%20Project%20Reg.," target="_blank">Admin Mail <i
+                <a href="mailto:studentplus@kongu.ac.in?subject=Attendance%20Project%20Reg.," target="_blank">studentplus@kongu.ac.in<i
                         class="envelope outline icon"></i></a>
             </div>
             <div style="float:right;color:pink;">
