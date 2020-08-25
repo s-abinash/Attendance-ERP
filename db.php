9c6958ecea5a85cc910200a2a85f40dd0b7e50fe
AjayR07 <ajayr.18cse@kongu.edu>
2020-08-25 17:06:11 +0530

    Kongu.edu setup files

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con= new mysqli("localhost:3306","admin_abinash","5&3c9qfdchRvmlTX","attendance");
-    if ($con->connect_error)
-    {
-        die("Connection failed: " . $con->connect_error);
-    }
-
-?>
--- trash/exec.sql
+++
@@ -1,1 +0,0 @@
-select * from `18-cse-a`;
