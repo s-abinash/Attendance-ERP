fb6e07cc5dd36a215e2fb4cc5e33ac957d9b937d
AJAY <ajayr.18cse@kongu.edu>
2020-08-31 11:49:26 +0530

    Update notEntered.php

--- notEntered.php
+++ notEntered.php
@@ -123,7 +123,7 @@ $temp='';
                             
                                 if(!empty($day_pd))
                                 {
-                                    if(($con->query("SELECT * FROM `alteration` WHERE `s1` LIKE '$staffid' AND `c1` LIKE '$code' AND`period` LIKE '$periods' AND `date` like '$date' "))->num_rows==0)
+                                    if(($con->query("SELECT * FROM `alteration` WHERE `s1` LIKE '$sid' AND `c1` LIKE '$code' AND`period` LIKE '$periods' AND `date` like '$date' "))->num_rows==0)
                                     {
                                         $dates+=array($date=>$day_pd);
                                     }
