134104b101db38244a9d1f8f4184de5b85683683
AJAY <ajayr.18cse@kongu.edu>
2020-09-14 19:49:55 +0530

    Update intimationmail.php

--- entity/intimationmail.php
+++ entity/intimationmail.php
@@ -372,16 +372,16 @@ while($row=mysqli_fetch_array($data))
                     </body>
                     </html>';
 //                         $mailto=$m[$ijk++];
-                        $mail->addAddress($mailto); 
-                        $mail->addReplyTo('studentplus@kongu.ac.in', 'KEC Student+');
-                        $mail->Subject = 'Attendance Pending';
-                        $mail->Body=$bodyContent;
-                        if(!$mail->send())
-                            echo $mailto.'Error';         
-                        else
-                            echo $mailto."Succesfull";
+//                         $mail->addAddress($mailto); 
+//                         $mail->addReplyTo('studentplus@kongu.ac.in', 'KEC Student+');
+//                         $mail->Subject = 'Attendance Pending';
+//                         $mail->Body=$bodyContent;
+//                         if(!$mail->send())
+//                             echo $mailto.'Error';         
+//                         else
+//                             echo $mailto."Succesfull";
                             
-                         // echo $bodyContent;
+                         echo $bodyContent;
             }
             
         }
