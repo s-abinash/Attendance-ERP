45870aee11daebbe1afb555ab4b53ee3c8eba9ca
AjayR07 <ajayr.18cse@kongu.edu>
2021-01-19 09:36:08 +0530

    revert

--- AJAX/handler.php
+++ AJAX/handler.php
@@ -2,51 +2,8 @@
     include_once("../db.php");
     session_start();
     $sid=$_SESSION["id"];
-//     include_once("./header.php");
-$sql="SELECT `code` FROM `course_list` where `status` LIKE 'active' AND `category` LIKE 'elective'";
-$res=$con->query($sql);
-$ele=array();
-while($row=mysqli_fetch_array($res))
-{
-    array_push($ele,$row['code']);
-}
-function ttfun($con,$ttname,$classname,$course)
-{
-    $sql="SELECT * FROM `$ttname` WHERE `class` LIKE '$classname'";
-    $res=$con->query($sql);
-    $day_per=array();
-    while($row=$res->fetch_assoc())
-    { 
-        $per=array();
-        foreach($row as $in=>$v)
-        {
-            if(strpos($v,$course)!==false)
-            {
-                array_push($per,$in);
-            } 
-        }
-        if(!empty($per))
-        {
-            $day_per+=array($row["day"]=>$per);
-        }   
-     }
-    return $day_per;
-}
-function timetablesfn($con,$tab,$code)
-{
-    $timetable=array();
-    $tt_res=$con->query("select * from tt_home order by id asc");
+    include_once("./header.php");
     
-    while($row=$tt_res->fetch_assoc())
-    {
-         $tt=array();
-         $tt["from"]=$row["from_date"];
-         $tt["to"]=$row["to_date"];
-         $tt["tt"]=ttfun($con,$row["rev"],$tab,$code);
-         array_push($timetable,$tt);
-    }
-    return $timetable;
-}
      if(isset($_POST["tab"]))
      {
           $tab=strtolower($_POST["tab"]);
