1bed7e5ab8751fc3a4432c5bf80888a91be6ad73
Abinash S <s.abinash@outlook.com>
2021-06-27 09:57:00 +0530

    Deployment XML with DB Updates

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$con= new mysqli("localhost:3306","admin_abinash","5&3c9qfdchRvmlTX","attendance");
-    if ($con->connect_error)
-    {
-        die("Connection failed: " . $con->connect_error);
-    }
+$con= new mysqli("localhost","attendance","attendance2021","attendance");
+if ($con->connect_error)
+{
+    die("Connection failed: " . $con->connect_error);
+}
 
 ?>
