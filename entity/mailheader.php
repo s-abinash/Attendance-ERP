f0b58a3990e6000a6e058fa601a45ba7a3a572fd
Abinash S <s.abinash@outlook.com>
2021-08-04 13:57:19 +0530

    Update mailheader.php

--- entity/mailheader.php
+++ entity/mailheader.php
@@ -2,8 +2,8 @@
 
 $Host = 'in-v3.mailjet.com';             // Specify main and backup SMTP servers
 $SMTPAuth = true;                     // Enable SMTP authentication
-$Username = '7018fe75de8b42ea3910a0231f4afb84';          // SMTP username
-$Password = '8032c5c02f1ac1cda864a0975f1f784f'; // SMTP password
+$Username = '';          // SMTP username
+$Password = ''; // SMTP password
 $SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
 $Port = 587; 
 ?>
